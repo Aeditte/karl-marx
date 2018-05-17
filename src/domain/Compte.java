05fdd3532a1038f597f6944f083474f15b816428
Leclair <karimleclair@gmail.com>
2018-05-17 16:01:10 +0200

    [WIP] class Compte

--- src/domain/Compte.java
+++ src/domain/Compte.java
@@ -1,6 +1,54 @@
+import java.util.UUID;
+import exception.InvalidCodeException;
 package domain;
 
-public class Compte {
+public abstract class Compte {
 
+    private long code;
+    private static int nbComptes;
+    protected double solde;
 
+    public void Compte() {
+
+    }
+
+    public long getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
+
+    private void geneId() {
+
+    }
+
+    private void verser(double montant) {
+        this.solde += montant;
+    }
+
+    private void retirer(double montant) {
+        if (montant > this.solde) {
+            throw new InvalidCodeException();
+        }
+
+        this.solde -= montant;
+    }
+
+    @Override
+    public String toString() {
+        return "Compte{" +
+                "code=" + code +
+                ", solde=" + solde +
+                '}';
+    }
 }
